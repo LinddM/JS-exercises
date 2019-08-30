17735cd7039f3ce5b989f73a9f34e86d6c9706c0
Maite de la Roca <delaroca.estrada21@gmail.com>
2019-08-30 15:40:07 -0500

    my-index-of exercise

--- workshops/02-loops/06-my-index-of/my-index-of.js
+++ workshops/02-loops/06-my-index-of/my-index-of.js
@@ -1,1 +1,16 @@
-// YOUR CODE BELOW
+function myIndexOf (source, searchValue, startIdx) {
+    index = 0
+    searchIndex = 0
+    coincidenceIndex = []
+    if (startIdx) {index = startIdx}
+    for (let i = index; i < source.length; i++) {
+        if (source[i] == searchValue[searchIndex]){
+            searchIndex++
+            coincidenceIndex.unshift(i)
+        }
+        if(searchIndex == searchValue.length){
+            return coincidenceIndex.pop()
+        }
+    }
+    return -1
+}
