662fe1a21808467d2f4ff96e4aa08388996df6f4
Maite de la Roca <delaroca.estrada21@gmail.com>
2019-08-30 14:56:03 -0500

    my-slice exercise

--- workshops/02-loops/05-my-slice/my-slice.js
+++ workshops/02-loops/05-my-slice/my-slice.js
@@ -1,1 +1,21 @@
-// YOUR CODE BELOW
+function mySlice(originalStr, startIdx, endIdx) {
+    let returnArray = []
+    cont = 0
+    if(!startIdx && !endIdx){
+        return originalStr
+    }
+    if (startIdx) {
+        for (let i = startIdx; i < originalStr.length; i++) {
+            returnArray[cont] = originalStr[i]
+            cont++
+        }
+        cont = startIdx
+    }
+    if (endIdx) {
+        while(endIdx >= cont - 1){
+            returnArray.pop()
+            cont++
+        }
+    }    
+    return returnArray.join('')
+}
