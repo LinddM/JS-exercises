ba2c414855386419af0e46fea4943c0f7a38023f
Maite de la Roca <delaroca.estrada21@gmail.com>
2019-08-30 16:07:35 -0500

    most-vowels exercise

--- workshops/02-loops/07-most-vowels/most-vowels.js
+++ workshops/02-loops/07-most-vowels/most-vowels.js
@@ -1,1 +1,19 @@
-// YOUR CODE BELOW
+function mostVowels (string) {
+    wordMaxVowels = ''
+    maxVowels = 0
+    string = string.replace('.','').split(' ')
+    for (let i = 0; i < string.length; i++){
+        vowels = 0
+        for (let j = 0; j < string[i].length; j++){
+            alp = string[i][j]
+            if (alp == 'a' || alp == 'e' || alp == 'i' || alp == 'o' || alp == 'u'){
+                vowels++
+            }
+        }
+        if(vowels > maxVowels){
+            maxVowels = vowels
+            wordMaxVowels = string[i]
+        }
+    }
+    return wordMaxVowels
+}
