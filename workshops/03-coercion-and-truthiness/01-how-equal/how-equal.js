52162eb29fa391620b7d4af32c8700a328478f10
Maite de la Roca <delaroca.estrada21@gmail.com>
2019-09-02 19:27:02 -0500

    coercion and truthiness module: how equal complete

--- workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
+++ workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
@@ -1,1 +1,9 @@
-// YOUR CODE BELOW
+function howEqual (argument1, argument2) {
+    if (argument1 === argument2) {
+        return 'strictly'
+    }else if (argument1 == argument2) {
+        return 'loosely'
+    }else{
+        return 'not equal'
+    }
+}
