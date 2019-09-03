85c3f1ff4906420460d56c47f30134039fdd74d0
Maite de la Roca <delaroca.estrada21@gmail.com>
2019-09-02 19:33:10 -0500

    is-truthy exercise

--- workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
+++ workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
@@ -1,1 +1,20 @@
-// YOUR CODE BELOW
+function isTruthy (argument) {
+    if (argument) {
+        return true
+    }
+    if (argument === false) {
+        return 'The boolean value false is falsey'
+    }
+    if (argument === null) {
+        return 'The null value is falsey'
+    }
+    if (argument === undefined) {
+        return 'undefined is falsey'
+    }
+    if (argument === 0) {
+        return 'The number 0 is falsey (the only falsey number)'
+    }
+    if (argument === '') {
+        return 'The empty string is falsey (the only falsey string)'
+    }
+}
