477538fa544a30d12cfa5fe1c9d7a9098ca7fc88
Maite de la Roca <delaroca.estrada21@gmail.com>
2019-09-04 23:03:43 -0500

    05-arrays-i, odd-couple exercise

--- workshops/05-arrays-i/01-odd-couple/odd-couple.js
+++ workshops/05-arrays-i/01-odd-couple/odd-couple.js
@@ -1,1 +1,11 @@
-// YOUR CODE BELOW
+function oddCouple (array) {
+    newArray = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 != 0) { newArray.push(array[i])}
+    }
+    if (newArray.length <= 2) {
+        return newArray
+    } else {
+        return newArray.slice(0,2)
+    }
+}
