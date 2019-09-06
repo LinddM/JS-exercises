7a9be4a8dc878c1ab4e4c55b54806e8fd63e53a9
Maite de la Roca <delaroca.estrada21@gmail.com>
2019-09-06 12:54:05 -0500

    05-arrays-i, 02-my-includes exercise complete

--- workshops/05-arrays-i/02-my-includes/my-includes.js
+++ workshops/05-arrays-i/02-my-includes/my-includes.js
@@ -1,1 +1,7 @@
-// YOUR CODE BELOW
+function myIncludes (array, searchValue) {
+    // if it doesn't exist inside the array we get an index of -1
+    if (array.indexOf(searchValue) != -1){
+        return true
+    }
+    return false
+}
